ba9b08171a0d40b35149a77a66b8ce93e95f7069
JVinay7 <vinayjorige7@gmail.com>
2023-09-27 16:30:27 +0530

    Upload all files into git

---
+++ config.py
@@ -0,0 +1,13 @@
+snowflake_user = "SNOWGPT"
+snowflake_password = "SnowGPT@202308"
+snowflake_account = "anblicksorg_aws.us-east-1"
+snowflake_warehouse = "SNOWGPT_WH"
+snowflake_database = "SNOWGPT_DB"
+snowflake_schema = "STG"
+stage_name = "snowgpt_s3_stage"
+
+api_key="741d647c-da49-4048-9d88-3b56e9e8e7f3"     
+environment="gcp-starter"  
+
+index_name="snowpineidx"
+
